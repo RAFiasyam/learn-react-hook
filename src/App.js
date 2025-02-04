bf5666f3506124079c88e44c9738bb4c35750977
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 14:56:03 +0700

    and you can do this too

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 
 function Phone(colors) {
-  return <h2>my phone color is {colors.color}.</h2>
+  return <h2>my phone color is {colors.color.color} {colors.color.model}.</h2>
 }
 
 function App() {
-  const colorName = "blue";
+  const colorInfo = { color: "blue", model: "Poco" };
   return (
     <div className="App-header">
       <h2>What your phone color?</h2>
-      <Phone color={colorName} />
+      <Phone color={colorInfo} />
     </div>
   );
 }
