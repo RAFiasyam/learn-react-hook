66800f7db9c1b7d7e1feba098784fba7ec93e7c7
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 13:55:46 +0700

    learn greating in here so idk if i call with this () its will be called as function but

--- src/App.js
+++ src/App.js
@@ -2,8 +2,17 @@ import './App.css';
 
 function App() {
 
+  const x = 5;
+  let text = 'goodbye';
+  if (x < 10) {
+    text = 'hello';
+  }
+
+  const myText = text;
+
   return (
     <div className="App-header">
+      <p>{myText}</p>
     </div>
   );
 }
