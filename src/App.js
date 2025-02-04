f7fa0eba88d8f1d46e94890e467dc78e8a515624
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 21:46:49 +0700

    learn textarea react placed in a value attribute

--- src/App.js
+++ src/App.js
@@ -3,40 +3,18 @@ import './App.css';
 
 
 function App() {
-  const [inputs, setInputs] = useState({});
+  const [textarea, setTextarea] = useState(
+    "The content of a textarea goes in the value attribute"
+  );
 
   const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setInputs(values => ({ ...values, [name]: value }))
-  }
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    alert(inputs)
+    setTextarea(event.target.value)
   }
 
   return (
     <div className="App-header">
       <form>
-        <label>Enter your name :
-          <input
-            type='text'
-            name='username'
-            placeholder='type name here...'
-            value={inputs.username || ""}
-            onChange={handleChange}
-          />
-        </label>
-        <label>Enter your age :
-          <input
-            type='number'
-            name='age'
-            value={inputs.age || ""}
-            onChange={handleChange}
-          />
-        </label>
-        <input type='submit' onClick={handleSubmit} />
+        <textarea value={textarea} onChange={handleChange} />
       </form>
     </div>
   );
