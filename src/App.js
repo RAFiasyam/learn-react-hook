fae41d460a4333f473896558e5569809c65e16e2
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 21:05:35 +0700

    yeah react form idk its semss like a HTML form the leason say it too

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App-header">
-      
+      <form>
+        <label>
+          <input type='text' placeholder='type shit...' />
+        </label>
+      </form>
     </div>
   );
 }
