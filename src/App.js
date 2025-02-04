f1811f8835ce3f7e6b7d5d436cef31db4732cc7c
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 21:17:33 +0700

    form with useState to keep track of each inputs value and probide a single source of truth for the entire application

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
+  const [name, setName] = useState('')
+
   return (
     <div className="App-header">
       <form>
         <label>
-          <input type='text' placeholder='type shit...' />
+          <input
+            type='text'
+            placeholder='type shit...'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
       </form>
     </div>
