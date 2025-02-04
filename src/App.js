9831028f6eaf5fea4219721cbf9c33ea733d50b9
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 15:11:53 +0700

    just clearing code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
 
-function Phone(colors) {
-  return <h2>my phone color is {colors.color.color} {colors.color.model}.</h2>
-}
-
 function App() {
-  const colorInfo = { color: "blue", model: "Poco" };
   return (
     <div className="App-header">
-      <h2>What your phone color?</h2>
-      <Phone color={colorInfo} />
+
     </div>
   );
 }
