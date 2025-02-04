cf930d24098332d3c3bfa7b7a1e211bc79068a29
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 21:40:23 +0700

    learn react form Multiple Input Fields

--- src/App.js
+++ src/App.js
@@ -3,22 +3,37 @@ import './App.css';
 
 
 function App() {
-  const [name, setName] = useState('')
+  const [inputs, setInputs] = useState({});
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs(values => ({ ...values, [name]: value }))
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(`That your name right? ${name}!?`)
+    alert(inputs)
   }
 
   return (
     <div className="App-header">
       <form>
-        <label>
+        <label>Enter your name :
           <input
             type='text'
+            name='username'
             placeholder='type name here...'
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={inputs.username || ""}
+            onChange={handleChange}
+          />
+        </label>
+        <label>Enter your age :
+          <input
+            type='number'
+            name='age'
+            value={inputs.age || ""}
+            onChange={handleChange}
           />
         </label>
         <input type='submit' onClick={handleSubmit} />
