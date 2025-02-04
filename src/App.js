ccd64f1a3abb4ab137d748a5dee965a33966fcac
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 15:31:36 +0700

    learn event onClick

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 
 function App() {
+  const say = () => {
+    alert("happy birthday!")
+  }
+
   return (
     <div className="App-header">
-
+      <button onClick={say}>hit this to say happy birthday</button>
     </div>
   );
 }
