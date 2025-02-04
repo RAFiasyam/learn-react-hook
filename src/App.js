5dd75bb9bc67d7988eec233689a574ef0e89ad01
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-04 20:57:14 +0700

    nothing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import './App.css';
 
-function Mobil(props) {
-  return <li>I have 1 brand car {props.brand}</li>
-}
 
 function App() {
-  const mobils = [
-    { id: 1, brand: 'Honda' },
-    { id: 2, brand: 'Toyota' },
-    { id: 3, brand: 'Nissan' }
-  ];
   return (
     <div className="App-header">
-      <ul>
-        {mobils.map((mobil) => <Mobil key={mobil.id} brand={mobil.brand} />)}
-      </ul>
+      
     </div>
   );
 }
