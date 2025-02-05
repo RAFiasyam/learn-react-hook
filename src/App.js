c686eec53b9225051c87528f6daf7c68a8884b17
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-05 16:53:56 +0700

    yay start learning react hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
+  const [color, setColor] = useState('blue')
+
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>my fav color is {color}</h1>
+      <div>
+        <button
+          style={{ background: 'red', color: 'white' }}
+          type='button'
+          onClick={() => setColor("red")}
+        >Red
+        </button>
+      </div>
+      <div>
+        <button
+          style={{ background: 'pink', color: 'black' }}
+          type='button'
+          onClick={() => setColor("pink")}
+        >Pink
+        </button>
+      </div>
+      <div>
+        <button
+          style={{ background: 'yellow', color: 'black' }}
+          type='button'
+          onClick={() => setColor("yellow")}
+        >Yellow
+        </button>
+      </div>
+      <div>
+        <button
+          style={{ background: 'blue', color: 'white' }}
+          type='button'
+          onClick={() => setColor("blue")}
+        >Blue
+        </button>
+      </div>
+      <div>
+        <button
+          style={{ background: 'green', color: 'white' }}
+          type='button'
+          onClick={() => setColor("green")}
+        >Green
+        </button>
+      </div>
     </div>
   );
 }
