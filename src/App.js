a9dee1eea47b50691b9945dbc8c4fb7d050d5fde
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-05 22:48:31 +0700

    this is learn bout effect clean up

--- src/App.js
+++ src/App.js
@@ -4,19 +4,21 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [calculation, setCalculation] = useState(0);
+  
 
 
   useEffect(() => {
-    setCalculation(() => count * 2);
-  }, [count]);
+    let timer = setTimeout(() => {
+      setCount((count) => count + 1);
+    }, 1000);
+
+    return () => clearTimeout(timer)
+  }, []);
 
 
   return (
     <div>
       <h1>Time rendered: {count}</h1>
-      <button onClick={() => setCount((c) => c + 1)} >+</button>
-      <p>Calculation : {calculation}</p>
     </div>
   );
 }
