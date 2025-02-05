1384c0443b73dea806d41bda02fbbd33a8548263
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-05 14:12:17 +0700

    nothing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import { useState } from 'react';
 import './App.css';
-import Todos from './Todos';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [todos] = useState(["todo 1", "todo 2"]);
-
-  const incremnt = () => {
-    setCount((c) => c + 1);
-  }
-
   return (
     <div className="App-header">
-      <Todos todos={todos} />
-      <hr />
-      <div>
-        Count: {count}
-        <button onClick={incremnt}>+</button>
-      </div>
+     
     </div>
   );
 }
