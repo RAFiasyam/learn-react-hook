c1e5beb4d94f725eba6c6bcd1739a1124c3ebccc
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-05 14:07:02 +0700

    this is problem we'll to fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './App.css';
+import Todos from './Todos';
 
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [todos] = useState(["todo 1", "todo 2"]);
+
+  const incremnt = () => {
+    setCount((c) => c + 1);
+  }
+
   return (
     <div className="App-header">
-      
+      <Todos todos={todos} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={incremnt}>+</button>
+      </div>
     </div>
   );
 }
