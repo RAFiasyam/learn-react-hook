d7ce4ac45207017756e859788f976c5bc8621bb3
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-05 22:46:35 +0700

    useEffect very cool we can fecthing data, directly updating the DOM, timers etc

--- src/App.js
+++ src/App.js
@@ -3,9 +3,20 @@ import './App.css';
 
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+
+
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]);
+
+
   return (
     <div>
-
+      <h1>Time rendered: {count}</h1>
+      <button onClick={() => setCount((c) => c + 1)} >+</button>
+      <p>Calculation : {calculation}</p>
     </div>
   );
 }
