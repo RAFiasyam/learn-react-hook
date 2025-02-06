626689673fb800753bdf70f245612e623bca836a
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-06 13:33:19 +0700

    Accessing DOM Elements

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import { useState, useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import './App.css';
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const count = useRef(0);
+  const inputElement = useRef();
 
-  useEffect(() => {
-    count.current = count.current + 1;
-  });
-
-  
+  const focusInput = () => {
+    inputElement.current.focus();
+  }
+ 
 
   return (
     <div>
-      <input
-        type='text'
-        value={inputValue}
-        onChange={((e) => setInputValue(e.target.value))}
-      />
-      <h3>Render Count : {count.current}</h3>
+      <input type='text' ref={inputElement} />
+      <button onClick={focusInput} >Focus Input</button>
     </div>
   );
 }
