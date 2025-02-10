4c44a6361b2dd029c84b030046740c00f24c11f6
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-10 15:05:06 +0700

    this custom hooks react, build a hook

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  })
 
   return (
     <>
-
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>
+        })}
     </>
   );
 }
