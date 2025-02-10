6f71fc0fd3ef23dadd53313ecb9311e28020a662
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-10 13:58:45 +0700

    Performance, useMemo Hook can be used to keep expensive

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
-import Todos from './Todos'
+
+const expensiveCalculation = (num) => {
+  console.log("calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num
+}
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [todos, setTodos] = useState([])
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const calculation = expensiveCalculation(count);
 
   const increment = () => {
     setCount((c) => c + 1);
   }
 
-  const addTodo = useCallback(() => {
+  const addTodo = () => {
     setTodos((t) => [...t, "New Todo"]);
-  }, []);
+  };
 
   return (
     <>
-      <Todos todos={todos} addTodo={addTodo}>
+      <div>
+        <div>
+          <h2>New Todos</h2>
+          {todos.map((todo, index) => {
+            return <p key={index}>{todo}</p>
+          })}
+          <button onClick={addTodo}>Add Todo</button>
+        </div>
         <hr />
         <div>
           Count: {count}
           <button onClick={increment}>+</button>
+          <h2>Expensive calculation</h2>
+          {calculation}
         </div>
-      </Todos>
+      </div>
     </>
   );
 }
