e2947d99120e67af24b73b897b135b72678f1354
RAFiasyam <rafi.asyam23@gmail.com>
2025-02-19 15:31:46 +0700

    learn useRef

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import { ContextExample } from './hooks/useContext';
 import { EffectExample } from './hooks/useEffect';
 import { ReducerExample } from './hooks/useReducer';
+import { RefExample } from './hooks/useRef';
 import { StateExample } from './hooks/useState';
 
 
 function App() {
   return (
     <>
-      <ReducerExample />
+    <RefExample />
     </>
   );
 }
